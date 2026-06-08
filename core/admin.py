5cd029f7244547335ad57dc53ad59485faaa9e95
Aadil Sakeer <aadilsakeer@gmail.com>
2026-06-09 00:33:46 +0530

    Added task administration

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,77 @@
 from django.contrib import admin
-from .models import Company, Branch
 
-admin.site.register(Company)
-admin.site.register(Branch)
+from .models import (
+    Company,
+    Branch,
+    Task,
+    TaskComment
+)
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'name'
+    )
+
+    search_fields = (
+        'name',
+    )
+
+
+@admin.register(Branch)
+class BranchAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'name',
+        'company'
+    )
+
+    list_filter = (
+        'company',
+    )
+
+    search_fields = (
+        'name',
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'title',
+        'assigned_to',
+        'assigned_by',
+        'status',
+        'priority',
+        'due_date',
+        'created_at'
+    )
+
+    list_filter = (
+        'status',
+        'priority'
+    )
+
+    search_fields = (
+        'title',
+        'description'
+    )
+
+
+@admin.register(TaskComment)
+class TaskCommentAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'task',
+        'user',
+        'created_at'
+    )
+
+    search_fields = (
+        'comment',
+    )
